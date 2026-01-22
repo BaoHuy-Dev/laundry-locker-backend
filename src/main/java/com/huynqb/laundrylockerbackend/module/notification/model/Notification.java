453094ef5778bd988c49d95c823f604a95efd0c6
BaoHuy <nqbhuy2004nt@gmail.com>
2026-01-22 19:01:38 +0700

    format noti

--- src/main/java/com/huynqb/laundrylockerbackend/module/notification/model/Notification.java
+++ src/main/java/com/huynqb/laundrylockerbackend/module/notification/model/Notification.java
@@ -3,15 +3,32 @@ package com.huynqb.laundrylockerbackend.module.notification.model;
 import com.huynqb.laundrylockerbackend.module.notification.enums.NotificationStatus;
 import com.huynqb.laundrylockerbackend.module.notification.enums.NotificationType;
 import com.huynqb.laundrylockerbackend.module.user.model.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
-import lombok.*;
 
 /** Notification entity for storing user notifications. */
 @Entity
-@Table(
-    name = "notifications",
-    indexes = {@Index(name = "idx_notifications_user_status", columnList = "user_id, status")})
+@Table(name = "notifications", indexes = {
+    @Index(name = "idx_notifications_user_status", columnList = "user_id, status") })
 @Getter
 @Setter
 @Builder
